6c638b2790e2b806ea34d652a0b2b9085685c196
Sunnuy <57054697+taeyangpark@users.noreply.github.com>
2020-04-16 22:04:26 +0900

    Add files via upload

--- clock.js
+++ clock.js
@@ -1,19 +1,76 @@
 const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("#ko");
+  clockTitle = clockContainer.querySelector("#Ko");
+  clockTitle1 = clockContainer.querySelector("#China");
+  clockTitle2 = clockContainer.querySelector("#America_NewYork");
+  clockTitle3 = clockContainer.querySelector("#Germany_Berlin");
 
+/*Korea Time*/
 function getTime() {
   const date = new Date();
-  const minutes = date.getMinutes();
   const hours = date.getHours();
+  const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+
+  clockTitle.innerText ="한국, 서울 : "+ `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+}
+
+
+/*China Time*/
+function getTime1() {
+  const date = new Date();
+  const hours = date.getHours()-1;
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+clockTitle1.innerText ="중국, 상하이 : "+ `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
+
+/*America_NewYork Time*/
+function getTime2() {
+  const date = new Date();
+  const hours = date.getHours()-13;
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+clockTitle2.innerText ="미국, 뉴옥 : " +  `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+
+/*Germany_Berlin Time*/
+function getTime3() {
+  const date = new Date();
+  const hours = date.getHours()-7;
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+clockTitle3.innerText ="독일, 베를린 : "+ `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+
+
+
+
+/**/
 function init() {
   getTime();
   setInterval(getTime, 1000);
+  getTime1();
+  setInterval(getTime1, 1000);
+  getTime2();
+  setInterval(getTime2, 1000);
+  getTime3();
+  setInterval(getTime3, 1000);
+
 }
 
 init();
