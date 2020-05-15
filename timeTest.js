26a15b75d1cebc4b52d2378e9ff29dfb721fe842
Sunnuy <57054697+taeyangpark@users.noreply.github.com>
2020-05-15 17:51:59 +0900

    Add files via upload

--- timeTest.js
+++ timeTest.js
@@ -1,2 +1,8 @@
-let m = moment();
-console.log(m);
+
+var b = moment('01/12/2016', 'DD/MM/YYYY', true).format()
+console.log(b);
+var c = moment.utc('2016-01-01T23:35:01').format();
+console.log(c);
+
+var a = moment('12 March, 2016', ['DDMMMMY', 'MMMMDDY']).format()
+console.log(a);
